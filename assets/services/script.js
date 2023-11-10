1671ee4422d45f4e5936b27b64fc3585e03b3c30
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-11-10 12:46:44 +0530

    Update script.js

--- assets/services/script.js
+++ assets/services/script.js
@@ -1,1 +1,14 @@
+// You can add JavaScript functionality here if needed
+// For example, dynamic content updates or interactions
+document.addEventListener('DOMContentLoaded', function () {
+    // Sample: Add a click event listener to the services list items
+    const servicesList = document.querySelectorAll('ul li');
+
+    servicesList.forEach(function (service) {
+        service.addEventListener('click', function () {
+            alert(`You selected: ${service.textContent}`);
+            // You can add more functionality here, such as displaying details of the selected service.
+        });
+    });
+});
 
