81559b8716612cc726349271b3fb0b6bf9861b6d
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-11-10 11:32:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Get a reference to the Gram Panchayat photo
+const gramPanchayatPhoto = document.querySelector('#gramPanchayatPhoto');
+
+// Add a click event listener to the Gram Panchayat photo
+gramPanchayatPhoto.addEventListener('click', () => {
+    alert('Welcome to Gram Panchayat!'); // Display an alert when clicked
+});
+
+// You can add more JavaScript functionality as needed
+// For example, handling form submissions, dynamic content updates, etc.
