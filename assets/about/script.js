fd3fe31d6b138414875b60722dda614602e89335
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-11-11 12:02:44 +0530

    Update script.js

--- assets/about/script.js
+++ assets/about/script.js
@@ -1,11 +1,22 @@
-// Get a reference to the Gram Panchayat photo
-const gramPanchayatPhoto = document.querySelector('#gramPanchayatPhoto');
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the main content area
+    var mainContent = document.querySelector('.main-content');
 
-// Add a click event listener to the Gram Panchayat photo
-gramPanchayatPhoto.addEventListener('click', () => {
-    alert('Welcome to Gram Panchayat!');
+    // Add a click event listener to the main content area
+    mainContent.addEventListener('click', function() {
+        // Change the background color on click
+        mainContent.style.backgroundColor = getRandomColor();
+    });
+
+    // Function to generate a random color
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 });
 
-// You can add more JavaScript functionality as needed
-// For example, handling form submissions, dynamic content updates, etc.
 
